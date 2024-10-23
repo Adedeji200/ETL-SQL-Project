985f39873b1561cb06f946cc54f27d0e02e25127
Adedeji200 <ajanibamimore01@gmail.com>
2024-10-23 14:44:59 +0100

    Updated Pipline Scipts

--- pipline.py
+++ pipline.py
@@ -19,14 +19,14 @@ engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{
 csv_folder= "Data"
 
 # loop through the csv file and load theem in PG
-def print_numbers_for_loop(n):
-    for file in os.lisdirs(csv_folder):
+def load_csv_to_postgres():
+    for file in os.listdir(csv_folder):
         if file.endswith(".csv"):
             table_name=file.split(".")[0]
-            df = pd.read_csv(r"{csv_folder}/{file}", encoding = 'ISO-8859-1' )
+            df = pd.read_csv(f"{csv_folder}/{file}", encoding = "ISO-8859-1")
             df.to_sql(table_name, engine, if_exists='replace', index=False)
             print(f"Table {table_name} loaded successfully ")
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     load_csv_to_postgres()
